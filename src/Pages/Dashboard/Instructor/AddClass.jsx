f051a29e765249a2bbbeaa304ea17db55de8571b
samsulalamasif <samsulalamasif@gmail.com>
2023-06-09 20:12:12 +0600

    react-awesome-reveal added

--- src/Pages/Dashboard/Instructor/AddClass.jsx
+++ src/Pages/Dashboard/Instructor/AddClass.jsx
@@ -9,6 +9,14 @@ const AddClass = () => {
                 <title>Photograph Space | Add Class</title>
             </Helmet>
             <SectionTitle title={"Add a Class"}></SectionTitle>
+            <form>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">What is your name?</span>
+                    </label>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </div>
+            </form>
         </div>
     );
 };
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,14 +1,21 @@
+import { Fade, Slide } from "react-awesome-reveal";
+
 const SectionTitle = ({ title }) => {
     return (
         <div className="mx-auto text-center my-16">
-            <p className="text-4xl text-sky-700 font-serif font-semibold mb-2">{title}</p>
-            <p className="text-sm font-mono font-semibold text-gray-700 py-4">
-                Photogrpy Space dolor sit amet consectetur adipisicing elit fdgs tr fs. <br />
-                Deserunt labrum quis officiis quidem tempore assumenda voluptat voluptatibusd eshsdf
-                <br />
-                nemo eum. Velit impedit ipsum vero maxime enim tempore itaqu, sed dicta.
+            <Slide direction="left" >
+                <p className="text-4xl text-sky-700 font-serif font-semibold mb-2">{title}</p>
+            </Slide>
+            <Fade delay={1e3} cascade damping={1e-1}>
+                <div className="text-sm font-mono font-semibold text-gray-700 py-4">
+                    <p>Photogrpy Space dolor sit amet consectetur adipisicing elit fdgs tr fs.</p>
+                    <p>Deserunt labrum quis officiis quidem tempore assumenda voluptat voluptatibusd eshsdf</p>
+                    <p>nemo eum. Velit impedit ipsum vero maxime enim tempore itaqu, sed dicta.</p>
 
-            </p>
+
+                </div>
+            </Fade>
+
         </div>
     );
 };
