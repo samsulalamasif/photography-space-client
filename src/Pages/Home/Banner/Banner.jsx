f6cb7af98afbeb48a2b77f6ac495fccd67aefc62
samsulalamasif <samsulalamasif@gmail.com>
2023-06-11 18:27:01 +0600

    banner added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,34 +1,61 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+
 import img1 from "../../../assets/banner/img1.jpg"
 import img2 from "../../../assets/banner/img2.jpg"
 import img3 from "../../../assets/banner/img3.jpg"
 import img4 from "../../../assets/banner/img4.jpg"
 import img5 from "../../../assets/banner/img5.jpg"
 
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './Banner.css';
+
+
+
 const Banner = () => {
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
     return (
-        <div>
-            <Carousel >
-                <div >
-                    <img src={img1} />
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                onAutoplayTimeLeft={onAutoplayTimeLeft}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img5} alt="" /></SwiperSlide>
+                <div className="autoplay-progress" slot="container-end">
+                    <svg viewBox="0 0 48 48" ref={progressCircle}>
+                        <circle cx="24" cy="24" r="20"></circle>
+                    </svg>
+                    <span ref={progressContent}></span>
                 </div>
-                <div>
-                    <img src={img2} />
-                </div>
-                <div>
-                    <img src={img3} />
-                </div>
-                <div>
-                    <img src={img4} />
-                </div>
-                <div>
-                    <img src={img5} />
-                </div>
-            </Carousel>
-        </div>
+            </Swiper>
+        </>
     );
-};
+}
+
 
 export default Banner;
