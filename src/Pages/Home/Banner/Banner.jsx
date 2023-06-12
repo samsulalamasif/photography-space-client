57724ba18c8c1ce6ae3702ba5dda35047d1e8ba7
samsulalamasif <samsulalamasif@gmail.com>
2023-06-12 23:31:00 +0600

    last added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,6 +7,11 @@ import img2 from "../../../assets/banner/img2.jpg"
 import img3 from "../../../assets/banner/img3.jpg"
 import img4 from "../../../assets/banner/img4.jpg"
 import img5 from "../../../assets/banner/img5.jpg"
+import img6 from "../../../assets/banner/img6.jpg"
+import img7 from "../../../assets/banner/img7.jpg"
+import img8 from "../../../assets/banner/img8.jpg"
+import img9 from "../../../assets/banner/img9.jpg"
+import img10 from "../../../assets/banner/img10.jpg"
 
 
 // Import Swiper styles
@@ -46,6 +51,11 @@ const Banner = () => {
                 <SwiperSlide><img src={img3} alt="" /></SwiperSlide>
                 <SwiperSlide><img src={img4} alt="" /></SwiperSlide>
                 <SwiperSlide><img src={img5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img6} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img7} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img8} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img9} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img10} alt="" /></SwiperSlide>
                 <div className="autoplay-progress" slot="container-end">
                     <svg viewBox="0 0 48 48" ref={progressCircle}>
                         <circle cx="24" cy="24" r="20"></circle>
